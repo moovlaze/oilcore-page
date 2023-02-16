0e6b8416e5bb348a9f439e6e1d0d3ac91770559c
Max <thewildstar.m@gmail.com>
2023-02-17 00:34:13 +0300

    added header and section intro

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -1,1 +1,11 @@
+export const openBurgerMenu = function () {
+	const page = document.querySelector(".page");
+	const menu = document.querySelector(".nav__top");
+	const burger = document.querySelector(".burger");
 
+	burger.addEventListener("click", function () {
+		menu.classList.toggle("open");
+		page.classList.toggle("page_lock");
+		burger.classList.toggle("active");
+	});
+};
