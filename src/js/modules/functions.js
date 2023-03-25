a1aaad222cab9dfc1955a233c605adb5c8f6be8d
Max <thewildstar.m@gmail.com>
2023-03-25 15:06:58 +0300

    add section map

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -9,3 +9,22 @@ export const openBurgerMenu = function () {
 		burger.classList.toggle("active");
 	});
 };
+
+export const map = function () {
+	const init = function () {
+		var myMap = new ymaps.Map("map", {
+			center: [55.755811, 37.617617],
+			zoom: 10,
+		});
+
+		myMap.controls.remove("geolocationControl");
+		myMap.controls.remove("searchControl");
+		myMap.controls.remove("trafficControl");
+		myMap.controls.remove("typeSelector");
+		myMap.controls.remove("fullscreenControl");
+		myMap.controls.remove("zoomControl");
+		myMap.controls.remove("rulerControl");
+		myMap.behaviors.disable(["scrollZoom"]);
+	};
+	ymaps.ready(init);
+};
